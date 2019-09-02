d1b78ef2001bc37d8cee7bca242092ddd43f6c5a
CN107 <cn107@CN0107.local>
2019-09-02 09:17:55 +0100

    Start of project

---
+++ script.js
@@ -0,0 +1,8 @@
+let player1 = prompt("What is your name, player 1?");
+let player2 = prompt("What is your name, player 2?");
+let rollButton = document.getElementById("rollButton");
+
+
+function rollDice() {
+   return Math.floor(Math.random() * 6 + 1);  
+}
