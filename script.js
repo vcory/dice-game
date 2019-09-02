52fdc12f9369ad6ff5bf7c3518547ae465c1a0e4
CN107 <cn107@CN0107.local>
2019-09-02 09:23:22 +0100

    extra div in html

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let player2 = prompt("What is your name, player 2?");
 let rollButton = document.getElementById("rollButton");
 
 
+
 function rollDice() {
    return Math.floor(Math.random() * 6 + 1);  
 }
